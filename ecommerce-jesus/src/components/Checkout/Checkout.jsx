3003143c28edac890d30e9b194d6993442b82f6f
jesuscxrod <rodriguezjerp@gmail.com>
2023-09-17 17:44:03 -0300

    Finalizando el componente Checkout

--- ecommerce-jesus/src/components/Checkout/Checkout.jsx
+++ ecommerce-jesus/src/components/Checkout/Checkout.jsx
@@ -10,74 +10,79 @@ import {
   writeBatch,
 } from "firebase/firestore";
 import { db } from "../../firebase/client";
-import CheckoutForm from "../CheckoutForm/CheckoutForm"; // Asegúrate de importar el componente CheckoutForm
+import CheckoutForm from "../CheckoutForm/CheckoutForm";
 
 const Checkout = () => {
   const [loading, setLoading] = useState(false);
   const [orderId, setOrderId] = useState("");
-
-  const { cart, total, clearCart } = useContext(CartContext);
+  const { cart, clearCart } = useContext(CartContext);
 
   const createOrder = async ({ name, phone, email }) => {
     setLoading(true);
-
+  
     try {
-      const objOrder = {
-        buyer: {
-          name,
-          phone,
-          email,
-        },
-        items: cart, // Cambiado 'item' a 'items'
-        total: total,
-        date: Timestamp.fromDate(new Date()),
-      };
-
       const batch = writeBatch(db);
-
       const outOfStock = [];
-
       const ids = cart.map((prod) => prod.id);
-
       const productsRef = collection(db, "products");
-
+  
       const productsAddedFromFirestore = await getDocs(
-        query(productsRef, where("id", "in", ids)) // Corregido el filtro 'where'
+        query(productsRef, where("id", "in", ids.map(id => id.toString())))
       );
-
+  
       const { docs } = productsAddedFromFirestore;
-
+  
       docs.forEach((doc) => {
         const dataDoc = doc.data();
-        const stockDb = dataDoc.stock; // Cambiado 'stockDb' a 'stock'
-
+        const stockDb = dataDoc.stock;
         const productAddedToCart = cart.find((prod) => prod.id === doc.id);
         const prodQuantity = productAddedToCart?.quantity;
-
+  
         if (stockDb >= prodQuantity) {
           batch.update(doc.ref, { stock: stockDb - prodQuantity });
         } else {
           outOfStock.push({ id: doc.id, ...dataDoc });
         }
       });
-
+  
       if (outOfStock.length === 0) {
+        const total = cart.reduce((total, item) => {
+          return total + item.price * item.quantity;
+        }, 0);
+  
+        if (isNaN(total)) {
+          console.error("El valor de total no es un número válido:", total);
+          return;
+        }
+  
+        const objOrder = {
+          buyer: {
+            name,
+            phone,
+            email,
+          },
+          items: cart,
+          total: total,
+          date: Timestamp.fromDate(new Date()),
+        };
+  
         await batch.commit();
-
-        const orderRef = collection(db, "orders");
-        const orderAdded = await addDoc(orderRef, objOrder);
-
+  
+        const orderCollection = collection(db, "orders");
+        const orderAdded = await addDoc(orderCollection, objOrder);
+  
         setOrderId(orderAdded.id);
         clearCart();
       } else {
         console.log("Error de stock");
       }
     } catch (error) {
-      console.log(error);
+      console.log("Error al crear la orden:", error);
     } finally {
       setLoading(false);
     }
   };
+  
 
   if (loading) {
     return <h1>Se está generando su orden</h1>;
